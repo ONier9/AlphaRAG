53f9089ed8f5a2897bf83c79abf63ef2a52c76f0
Luplup23 <oliwia.nieradzik@proton.me>
2025-05-08 16:52:58 +0200

    Chatbot beginnings

--- Chat.py
+++ Chat.py
@@ -1,13 +1,27 @@
 import torch
-from transformers import pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline, BitsAndBytesConfig
 
 model_id = "meta-llama/Llama-3.2-3B"
 
+quant_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_compute_dtype=torch.bfloat16,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_use_double_quant=True
+)
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    device_map="auto",
+    quantization_config=quant_config,
+)
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
 pipe = pipeline(
-    "text-generation", 
-    model=model_id, 
-    torch_dtype=torch.bfloat16, 
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
     device_map="auto"
 )
 
-pipe("The key to life is")
+output=pipe("Jesteś pomocnikiem dla użytkownika, który będzie odpowiadał na jego pytania zgodnie z parametrami.", max_new_tokens=200)
+
